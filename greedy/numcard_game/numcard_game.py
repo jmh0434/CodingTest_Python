8d64c6b526656478ee534f8fd20f583ef025edb7
jmh0434 <jeongmh0434@gmail.com>
2021-07-11 16:27:25 +0900

    숫자 카드 게임

--- greedy/numcard_game/numcard_game.py
+++ greedy/numcard_game/numcard_game.py
@@ -1,12 +1,16 @@
+# N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
 
 result = 0
 
+# 한 줄씩 입력받아 계산
 for i in range(n):
     data = list(map(int, input().split()))
+
+    # 현재 줄에서 가장 작은 수 찾기
     min_value = min(data)
 
+    # 가장 작은 수들 중에서 가장 큰 값 찾기
     result = max(result, min_value)
 
-print(data)
 print(result)
