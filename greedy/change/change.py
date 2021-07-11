1b6c9ca9301541cc3b44c6bca492b2692af8d60f
jmh0434 <jeongmh0434@gmail.com>
2021-07-11 16:27:09 +0900

    거스름돈 문제

--- greedy/change/change.py
+++ greedy/change/change.py
@@ -1,10 +1,10 @@
-n = 1260
-count = 0
+n = 1260  # 잔돈
+count = 0  # 동전의 개수
 
-coin_types = [500, 100, 50, 10]
+coin_types = [500, 100, 50, 10]  # 동전의 종류
 
 for coin in coin_types:
-    count += n // coin
-    n %= coin
+    count += n // coin  # 동전의 개수 계산
+    n %= coin  # 남은 잔돈
 
 print(count)
