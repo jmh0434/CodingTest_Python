7455282c467949065ffb883c847bd5296ce94af7
jmh0434 <jeongmh0434@gmail.com>
2021-07-11 16:27:34 +0900

    큰 수의 법칙

--- greedy/big_number/big_number.py
+++ greedy/big_number/big_number.py
@@ -1,16 +1,19 @@
-n, m, k = map(int,input().split())
-data = list(map(int,input().split()))
+# N, M, K를 공백으로 구분하여 입력받기
+n, m, k = map(int, input().split())
 
-data.sort()
-print (data)
-first = data[n - 1]
-second = data[n - 2]
+# N개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
 
+data.sort()  # 입력받은 수들 정렬
+first = data[n - 1]  # 가장 큰 수
+second = data[n - 2]  # 두 번째로 큰 수
+
+# 가장 큰 수가 더해지는 횟수 계산
 count = int(m / (k + 1)) * k
 count = count + m % (k + 1)
 
 result = 0
-result += (count) * first
-result += (m - count) * second
+result += (count) * first  # 가장 큰 수 더하기
+result += (m - count) * second  # 두 번째로 큰 수 더하기
 
 print(result)
